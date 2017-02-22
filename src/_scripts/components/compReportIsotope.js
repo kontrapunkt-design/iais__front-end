af9d6acda4d8513c83b690bf08b7d096af430c2b
Halei Liu <hli@kontrapunkt.com>
2017-02-22 15:08:01 +0100

    isotop filter function

--- src/_scripts/components/compReportIsotope.js
+++ src/_scripts/components/compReportIsotope.js
@@ -10,6 +10,10 @@ export default()=>{
   let reportSection = document.querySelector('.comp__section--report_list');
   // let reportItem = document.querySelector('.comp__section--report_list .comp__thumbnail--report');
 
+  function _getFilterValue(x){
+    return x.options[(x.selectedIndex)].value;
+  }
+
   imagesLoaded(reportSection, function() {
     let iso = new Isotope( '.report-isotype-grid', {
       // options
@@ -29,6 +33,20 @@ export default()=>{
       }
     });
 
+    document.getElementById('isotopeFilter').addEventListener('change', function(){
+      let isoFilter = document.getElementById('isotopeFilter');
+      let isoFilterValue = _getFilterValue(isoFilter);
+      iso.arrange({
+        // item element provided as argument
+        filter: '.'+isoFilterValue
+      });
+
+      console.log(isoFilterValue);
+    })
+
+
   });
+
+
 
 }
