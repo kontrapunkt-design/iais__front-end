744e734419b6997f7c751bca839cd74bbee2b2ae
Halei Liu <hli@kontrapunkt.com>
2017-03-14 16:31:39 +0100

    loading animation depends on window sizes and page type

--- src/_scripts/main--frontpage--A.js
+++ src/_scripts/main--frontpage--A.js
@@ -3,9 +3,71 @@
 
 'use strict';
 import compFrontheroA from './components/compFrontheroA';
+import ProgressBar from 'progressbar.js';
+import {loading} from './components/globalAnimation';
 
 $(() => {
-  setTimeout(function(){
+
+  var query = Modernizr.mq('(min-width: 640px)');
+  if (query) {
+    // the browser window is larger than 900px
+    document.getElementsByTagName( 'html' )[0].style.overflow = 'hidden';
+    let progressBar = new ProgressBar.Line(
+      '#loading-container',
+      {
+        color: '#ffffff',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 1,
+        trailWidth: 1,
+        trailColor: '#26387C',
+        easing: 'easeInOut',
+        text:{
+            style:{
+              color:'#26387C',
+              position: 'absolute',
+              top: '0',
+              zIndex: '9999',
+              fontSize: '40px',
+              fontWeight : 'bold',
+              // left: '50%',
+              top: '0',
+            }
+        },
+        from: {
+          color: '#000',
+        },
+        to: {
+          color: '#000',
+        },
+        // Set default step function for all animate calls
+        step: (state, bar)=> {
+          let value = Math.round(bar.value() * 100);
+          let vpW = window.innerWidth;
+          // bar.setText(Math.round(bar.value() * 100) + ' %');
+          if (value === 0) {
+          } else {
+            bar.setText(value);
+            document.querySelector('#cover .progressbar-text').style.left = vpW * (value/100) + 'px';
+            // console.log(value);
+          }
+        }
+      }
+    )
+
+    progressBar.animate(1, {
+      duration: 2000
+    }, function() {
+        loading().animation.play();
+        // console.log('Animation has finished');
+    });
+
+    setTimeout(function(){
+      compFrontheroA();
+    }, 3000)
+
+  } else {
+    loading().animation.play();
     compFrontheroA();
-  }, 3000)
+  }
 });
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -5,9 +5,7 @@
 
 import $ from 'jquery';
 import gsap from 'gsap';
-import ProgressBar from 'progressbar.js';
 import modernizr from './vendors/modernizr-custom';
-import {loading} from './components/globalAnimation';
 import compNavigation from './components/compNavigation';
 import compSearch from './components/compSearch';
 import compTooltips from './components/compTooltips';
@@ -16,57 +14,6 @@ import svg4everybody from 'svg4everybody';
 $(() => {
   // new Link(); // Activate Link modules logic
   svg4everybody();
-  document.getElementsByTagName( 'html' )[0].style.overflow = 'hidden';
-  let progressBar = new ProgressBar.Line(
-    '#loading-container',
-    {
-      color: '#ffffff',
-      // This has to be the same size as the maximum width to
-      // prevent clipping
-      strokeWidth: 1,
-      trailWidth: 1,
-      trailColor: '#26387C',
-      easing: 'easeInOut',
-      text:{
-          style:{
-            color:'#26387C',
-            position: 'absolute',
-            top: '0',
-            zIndex: '9999',
-            fontSize: '40px',
-            fontWeight : 'bold',
-            // left: '50%',
-            top: '0',
-          }
-      },
-      from: {
-        color: '#000',
-      },
-      to: {
-        color: '#000',
-      },
-      // Set default step function for all animate calls
-      step: (state, bar)=> {
-        let value = Math.round(bar.value() * 100);
-        let vpW = window.innerWidth;
-        // bar.setText(Math.round(bar.value() * 100) + ' %');
-        if (value === 0) {
-        } else {
-          bar.setText(value);
-          document.querySelector('#cover .progressbar-text').style.left = vpW * (value/100) + 'px';
-          // console.log(value);
-        }
-      }
-    }
-  )
-
-  progressBar.animate(1, {
-    duration: 2000
-  }, function() {
-      loading().animation.play();
-      // console.log('Animation has finished');
-  });
-
   compSearch();
   compNavigation();
   compTooltips();
